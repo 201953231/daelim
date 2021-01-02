731f51736a16a523b522cdeb9b1062c93af51c59
201953231 <201953231@email.daelim.ac.kr>
2021-01-02 10:04:19 +0900

    v6 isPrime int to boolean

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -6,20 +6,23 @@ public class PrimeNumber {
         Scanner scv = new Scanner(System.in);
         int n = scv.nextInt();
 
-        int count  = 0;
+        // int count = 0;
+        boolean isPrime = true;
 
         for(int k=2; k<n; k++){
             if(n%k == 0){
-                count ++;
+                //count ++; // count = count + 1;
+                isPrime = false;
                 break;
             }
             System.out.println(k);
         }
 
-        if(count == 0){
-            System.out.println(n + "is Prime Number!");
+        // if(count == 0)
+        if(isPrime){
+            System.out.println(n + " is Prime Number!");
         }else{
-            System.out.println(n + "is Not Prime Number");
+            System.out.println(n + " is Not Prime Number");
         }
     }
 }
